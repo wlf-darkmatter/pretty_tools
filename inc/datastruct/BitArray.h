ecf8df4373a5b989e2f337d96e4c8d5a8a2b348b
docker <docker@x-contion.top>
2023-12-06 22:16:33 +0800

    Refactor BitArray class and move writeBit and readBit functions to the pretty_tools namespace

--- inc/datastruct/BitArray.h
+++ inc/datastruct/BitArray.h
@@ -5,144 +5,150 @@
 #include <string.h>
 #include <exception>
 #include <stdexcept>
-/**
-    在map的position位置写入bit
-**/
-bool writeBit(unsigned char *, int, bool);
 
-/**
-   读取map的position位置的bit数据
-**/
-bool readBit(unsigned char *, int);
-
-/* 三个用于 BitArray 的静态常量 */
-static const size_t c_initBitsCapacity = 80U;
-static const double c_increaseCapacity = 1.5;
-static const double c_maxAllowedOutOfBound = 3.0;
-
-class BitArray;
-
-class Bit
+namespace pretty_tools
 {
-private:
-    BitArray *m_bits;
-    int m_position;
+    /**
+        在map的position位置写入bit
+    **/
+    bool writeBit(unsigned char *, int, bool);
 
-public:
-    Bit() : m_bits(nullptr), m_position(0) {}
-    Bit(BitArray *bits, int position);
-    Bit &operator=(bool bit);
-    operator bool();
-};
+    /**
+       读取map的position位置的bit数据
+    **/
+    bool readBit(unsigned char *, int);
 
-/**
-    BitArray 可以对比特位进行直接操作，通过构造方法或者setData()传入一个字符指针之后就可以将BitArray视作一个由比特组成的数组
-    set() 以及 get() 方法封装了对位进行操作的两个最主要的函数
-**/
-class BitArray
-{
-    // 类型，常量定义区
-    /*    无符号字符类型 */
-    typedef unsigned char uchar;
-    inline size_t BitsToBytes(size_t bits) { return (bits - 1) / 8 + 1; }
-    inline size_t BytesToBits(size_t bytes) { return 8 * bytes; }
+    /* 三个用于 BitArray 的静态常量 */
+    static const size_t c_initBitsCapacity = 80U;
+    static const double c_increaseCapacity = 1.5;
+    static const double c_maxAllowedOutOfBound = 3.0;
 
-public:
-    /**
-    默认构造函数，创建一个默认大小 c_initBitsCapacity 的比特数组
-    **/
-    BitArray();
-    BitArray &operator=(const BitArray &bits);
-    BitArray &operator=(BitArray &&bits);
-    BitArray(const BitArray &bits);
-    BitArray(BitArray &&bits);
-    /**
-    创建一个长为bitsLength，最大容量为bitsCapacity的比特数组
-    **/
-    BitArray(size_t bitsLength, size_t bitsCapacity = 0U);
-    /**
-    根据现有字符数组创建一个比特数组
-        data: 现有的字符数组
-        bitsLength: 该字符数组有效的比特位长度，创建之后的最大容量为 8*((bitsLength-1)/8+1)
-        isClear: 该字符数组是否进行清零
-        isOwns: 是否允许比特数组获得对该字符数组的控制权，若为true则在析构或其他恰当时机将会进行内存释放
-    **/
-    BitArray(uchar *data, int bitsLength, bool isClear = false, bool isOwns = true);
-    ~BitArray();
-    bool operator==(BitArray &bits);
-    /**
-    获得position位置的真值，
-    **/
-    Bit operator[](int position) { return Bit(this, position); }
-    Bit at(int position) { return Bit(this, position); }
-    /**
-    获得position位置的真值，有效范围为 [-(int)getBitSize(),getBitSize()),超出将抛出异常
-    **/
-    bool get(int position);
-    /**
-    获得比特数组的底层字节数据，该数组的有效长度可由 getBitSize()/getByteSize() 得到
-    **/
-    uchar *getData() { return m_data; }
-    /**
-    根据现有字符数组更新比特数组，原有的数据将根据m_owns的真值来决定是否释放
-        data: 现有的字符数组
-        bitsLength: 该字符数组有效的比特位长度，创建之后的最大容量为 8*((bitsLength-1)/8+1)
-        isClear: 该字符数组是否进行清零
-        isOwns: 是否允许比特数组获得对该字符数组的控制权，若为true则在析构或其他恰当时机将会进行内存释放
-    **/
-    void setData(uchar *data, int bitsLength, bool isClear = false, bool isOwns = true);
-    /**
-    对比特数组的position位置进行数据更新
-        position: 访问位置，以0为起点，合法范围为 [-(int)getBitSize(),getBitSize())，超出将可能抛出异常
-        bit: 将要更新的真值
-        isAllowOutOfRange: 是否允许在适当时机进行数据扩增，并且最大扩充倍数为c_maxAllowOutOfRange，默认是不允许的
-        isAllowOutOfSize: 是否允许当超出当前长度，但是并未超出容量时进行自动扩张,默认是允许的
-        isAllowToInfinite： 是否允许大小无限大，默认是不允许的
-    **/
-    bool set(int position, bool bit, bool isAllowOutOfRange = false, bool isAllowOutofSize = true, bool isAllowToInfinite = false);
-    /**
-    设置比特数组的有效长度，单位：比特
-    如果超出容量，将会进行扩容，扩增后的容量为 c_increaseCapacity*newBitsLength
-    **/
-    size_t setBitSize(size_t newBitsLength);
-    /**
-    设置比特数组的最大容量，单位：比特，但是将会以8为基本单位对齐
-    只要底层数据的字节数与新容量的占用字节数不同，就将重新分配内存，并且获得对新内存的支配权
-    **/
-    size_t setBitCapacity(size_t newBitsCapacity);
-    /**
-    获得比特数组的有效比特长度，单位为：比特
-    **/
-    size_t getBitSize() { return m_bitsLength; }
-    /**
-    获得比特数组的最大比特容量，单位为：比特
-    **/
-    size_t getBitCapacity() { return m_bitsCapacity; }
-    /**
-    获得比特数组的有效字节长度，单位为：字节
-    **/
-    size_t getByteSize() { return BitsToBytes(m_bitsLength); }
-    /**
-    获得比特数组的最大字节容量，单位为：字节
-    **/
-    size_t getByteCapacity() { return BitsToBytes(m_bitsCapacity); }
-    /**
-    判断是否拥有对底层数组的控制权
-    **/
-    bool isOwns() { return m_owns; }
-    /**
-    设置是否拥有对底层数组的控制权
-    **/
-    bool setOwns(bool owns);
+    class BitArray;
 
-private:
-    /* 底层数据数组    */
-    uchar *m_data;
-    /* 比特数组的有效长度 */
-    size_t m_bitsLength;
-    /* 比特数组的最大比特位容量，该值将永远是8的倍数 */
-    size_t m_bitsCapacity;
-    /* 代表比特数组是否拥有对m_data的控制权，拥有控制权则将在适当时机对其进行释放 */
-    bool m_owns;
-};
+    class Bit
+    {
+    private:
+        BitArray *m_bits;
+        int m_position;
+
+    public:
+        Bit() : m_bits(nullptr), m_position(0) {}
+        Bit(BitArray *bits, int position);
+        Bit &operator=(bool bit);
+        operator bool();
+    };
+
+    /**
+        BitArray 可以对比特位进行直接操作，通过构造方法或者setData()传入一个字符指针之后就可以将BitArray视作一个由比特组成的数组
+        set() 以及 get() 方法封装了对位进行操作的两个最主要的函数
+    **/
+    class BitArray
+    {
+        // 类型，常量定义区
+        /*    无符号字符类型 */
+        typedef unsigned char uchar;
+        inline size_t BitsToBytes(size_t bits) { return (bits - 1) / 8 + 1; }
+        inline size_t BytesToBits(size_t bytes) { return 8 * bytes; }
+
+    public:
+        /**
+        默认构造函数，创建一个默认大小 c_initBitsCapacity 的比特数组
+        **/
+        BitArray();
+        BitArray &operator=(const BitArray &bits);
+        BitArray &operator=(BitArray &&bits);
+        BitArray(const BitArray &bits);
+        BitArray(BitArray &&bits);
+        /**
+        创建一个长为bitsLength，最大容量为bitsCapacity的比特数组
+        **/
+        BitArray(size_t bitsLength, size_t bitsCapacity = 0U);
+        /**
+        根据现有字符数组创建一个比特数组
+            data: 现有的字符数组
+            bitsLength: 该字符数组有效的比特位长度，创建之后的最大容量为 8*((bitsLength-1)/8+1)
+            isClear: 该字符数组是否进行清零
+            isOwns: 是否允许比特数组获得对该字符数组的控制权，若为true则在析构或其他恰当时机将会进行内存释放
+        **/
+        BitArray(uchar *data, int bitsLength, bool isClear = false, bool isOwns = true);
+        ~BitArray();
+        bool operator==(BitArray &bits);
+        /**
+        获得position位置的真值，
+        **/
+        Bit operator[](int position) { return Bit(this, position); }
+        Bit at(int position) { return Bit(this, position); }
+        /**
+        获得position位置的真值，有效范围为 [-(int)getBitSize(),getBitSize()),超出将抛出异常
+        **/
+        bool get(int position);
+        /**
+        获得比特数组的底层字节数据，该数组的有效长度可由 getBitSize()/getByteSize() 得到
+        **/
+        uchar *getData() { return m_data; }
+        /**
+        根据现有字符数组更新比特数组，原有的数据将根据m_owns的真值来决定是否释放
+            data: 现有的字符数组
+            bitsLength: 该字符数组有效的比特位长度，创建之后的最大容量为 8*((bitsLength-1)/8+1)
+            isClear: 该字符数组是否进行清零
+            isOwns: 是否允许比特数组获得对该字符数组的控制权，若为true则在析构或其他恰当时机将会进行内存释放
+        **/
+        void setData(uchar *data, int bitsLength, bool isClear = false, bool isOwns = true);
+        /**
+        对比特数组的position位置进行数据更新
+            position: 访问位置，以0为起点，合法范围为 [-(int)getBitSize(),getBitSize())，超出将可能抛出异常
+            bit: 将要更新的真值
+            isAllowOutOfRange: 是否允许在适当时机进行数据扩增，并且最大扩充倍数为c_maxAllowOutOfRange，默认是不允许的
+            isAllowOutOfSize: 是否允许当超出当前长度，但是并未超出容量时进行自动扩张,默认是允许的
+            isAllowToInfinite： 是否允许大小无限大，默认是不允许的
+        **/
+        bool set(int position, bool bit, bool isAllowOutOfRange = false, bool isAllowOutofSize = true, bool isAllowToInfinite = false);
+        /**
+        设置比特数组的有效长度，单位：比特
+        如果超出容量，将会进行扩容，扩增后的容量为 c_increaseCapacity*newBitsLength
+        **/
+        size_t setBitSize(size_t newBitsLength);
+        /**
+        设置比特数组的最大容量，单位：比特，但是将会以8为基本单位对齐
+        只要底层数据的字节数与新容量的占用字节数不同，就将重新分配内存，并且获得对新内存的支配权
+        **/
+        size_t setBitCapacity(size_t newBitsCapacity);
+        /**
+        获得比特数组的有效比特长度，单位为：比特
+        **/
+        size_t getBitSize() { return m_bitsLength; }
+        /**
+        获得比特数组的最大比特容量，单位为：比特
+        **/
+        size_t getBitCapacity() { return m_bitsCapacity; }
+        /**
+        获得比特数组的有效字节长度，单位为：字节
+        **/
+        size_t getByteSize() { return BitsToBytes(m_bitsLength); }
+        /**
+        获得比特数组的最大字节容量，单位为：字节
+        **/
+        size_t getByteCapacity() { return BitsToBytes(m_bitsCapacity); }
+        /**
+        判断是否拥有对底层数组的控制权
+        **/
+        bool isOwns() { return m_owns; }
+        /**
+        设置是否拥有对底层数组的控制权
+        **/
+        bool setOwns(bool owns);
+
+    private:
+        /* 底层数据数组    */
+        uchar *m_data;
+        /* 比特数组的有效长度 */
+        size_t m_bitsLength;
+        /* 比特数组的最大比特位容量，该值将永远是8的倍数 */
+        size_t m_bitsCapacity;
+        /* 代表比特数组是否拥有对m_data的控制权，拥有控制权则将在适当时机对其进行释放 */
+        bool m_owns;
+    };
+
+}
+
 #endif
--- src/datastruct/BitArray.cc
+++ src/datastruct/BitArray.cc
@@ -1,325 +1,328 @@
 #include "BitArray.h"
 
-
-BitArray::BitArray()
+namespace pretty_tools
 {
-    m_owns = true;
-    m_bitsLength = 0;
-    if (c_initBitsCapacity == 0)
+
+    BitArray::BitArray()
     {
-        m_data = nullptr;
-        m_bitsCapacity = 0;
+        m_owns = true;
+        m_bitsLength = 0;
+        if (c_initBitsCapacity == 0)
+        {
+            m_data = nullptr;
+            m_bitsCapacity = 0;
+        }
+        else
+        {
+            size_t t_bytesLength = BitsToBytes(c_initBitsCapacity);
+            m_data = new uchar[t_bytesLength];
+            memset(m_data, 0, t_bytesLength);
+            if (!m_data)
+            {
+                // 内存分配失败逻辑
+                throw std::bad_alloc(); //("can't allow memory!");
+            }
+            m_bitsCapacity = 8 * t_bytesLength;
+        }
     }
-    else
+
+    BitArray::BitArray(BitArray &&bits)
     {
-        size_t t_bytesLength = BitsToBytes(c_initBitsCapacity);
-        m_data = new uchar[t_bytesLength];
-        memset(m_data, 0, t_bytesLength);
+        m_data = bits.m_data;
+        m_owns = true;
+        m_bitsCapacity = bits.m_bitsCapacity;
+        m_bitsLength = bits.m_bitsLength;
+        bits.m_owns = false;
+        bits.m_data = nullptr;
+    }
+
+    BitArray::BitArray(const BitArray &bits)
+    {
+        *this = bits;
+    }
+
+    BitArray &BitArray::operator=(const BitArray &bits)
+    {
+        m_data = bits.m_data;
+        m_owns = true;
+        m_bitsCapacity = bits.m_bitsCapacity;
+        m_bitsLength = bits.m_bitsLength;
+        uchar *t_data = new uchar[BitsToBytes(m_bitsCapacity)];
+        memcpy(t_data, m_data, BitsToBytes(m_bitsCapacity));
+        m_data = t_data;
+        return *this;
+    }
+
+    BitArray &BitArray::operator=(BitArray &&bits)
+    {
+        m_data = bits.m_data;
+        m_owns = true;
+        m_bitsCapacity = bits.m_bitsCapacity;
+        m_bitsLength = bits.m_bitsLength;
+        bits.m_owns = false;
+        bits.m_data = nullptr;
+        return *this;
+    }
+
+    BitArray::BitArray(size_t bitsLength, size_t bitsCapacity)
+    {
+        /**
+            整体思路：如果 bitsCapacity==0 ，那么默认容量将以 bitsLength乘以默认系数扩增
+        **/
+        //
+        m_bitsLength = bitsLength;
+        m_owns = true;
+        size_t t_fact_bitsCapacity = bitsCapacity;
+        if (t_fact_bitsCapacity < bitsLength)
+        {
+            t_fact_bitsCapacity = size_t(c_increaseCapacity * bitsLength);
+        }
+        size_t t_fact_bytesCapacity = BitsToBytes(t_fact_bitsCapacity);
+        m_bitsCapacity = 8 * t_fact_bytesCapacity;
+        m_data = new uchar[t_fact_bytesCapacity];
         if (!m_data)
         {
             // 内存分配失败逻辑
             throw std::bad_alloc(); //("can't allow memory!");
         }
-        m_bitsCapacity = 8 * t_bytesLength;
+        memset(m_data, 0, t_fact_bytesCapacity);
     }
-}
 
-BitArray::BitArray(BitArray &&bits)
-{
-    m_data = bits.m_data;
-    m_owns = true;
-    m_bitsCapacity = bits.m_bitsCapacity;
-    m_bitsLength = bits.m_bitsLength;
-    bits.m_owns = false;
-    bits.m_data = nullptr;
-}
-
-BitArray::BitArray(const BitArray &bits)
-{
-    *this = bits;
-}
-
-BitArray &BitArray::operator=(const BitArray &bits)
-{
-    m_data = bits.m_data;
-    m_owns = true;
-    m_bitsCapacity = bits.m_bitsCapacity;
-    m_bitsLength = bits.m_bitsLength;
-    uchar *t_data = new uchar[BitsToBytes(m_bitsCapacity)];
-    memcpy(t_data, m_data, BitsToBytes(m_bitsCapacity));
-    m_data = t_data;
-    return *this;
-}
-
-BitArray &BitArray::operator=(BitArray &&bits)
-{
-    m_data = bits.m_data;
-    m_owns = true;
-    m_bitsCapacity = bits.m_bitsCapacity;
-    m_bitsLength = bits.m_bitsLength;
-    bits.m_owns = false;
-    bits.m_data = nullptr;
-    return *this;
-}
-
-BitArray::BitArray(size_t bitsLength, size_t bitsCapacity)
-{
-    /**
-        整体思路：如果 bitsCapacity==0 ，那么默认容量将以 bitsLength乘以默认系数扩增
-    **/
-    //
-    m_bitsLength = bitsLength;
-    m_owns = true;
-    size_t t_fact_bitsCapacity = bitsCapacity;
-    if (t_fact_bitsCapacity < bitsLength)
+    BitArray::~BitArray()
     {
-        t_fact_bitsCapacity = size_t(c_increaseCapacity * bitsLength);
-    }
-    size_t t_fact_bytesCapacity = BitsToBytes(t_fact_bitsCapacity);
-    m_bitsCapacity = 8 * t_fact_bytesCapacity;
-    m_data = new uchar[t_fact_bytesCapacity];
-    if (!m_data)
-    {
-        // 内存分配失败逻辑
-        throw std::bad_alloc(); //("can't allow memory!");
-    }
-    memset(m_data, 0, t_fact_bytesCapacity);
-}
-
-BitArray::~BitArray()
-{
-    if (m_owns && m_data != nullptr)
-        delete[] m_data;
-}
-
-bool BitArray::operator==(BitArray &bits)
-{
-    if (m_bitsLength != bits.m_bitsLength)
-        return false;
-    for (int i = 0; i < m_bitsLength; i++)
-    {
-        if (get(i) != bits.get(i))
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-BitArray::BitArray(unsigned char *data, int bitsLength, bool isClear, bool isOwns)
-{
-    m_data = data;
-    m_bitsLength = bitsLength;
-    m_bitsCapacity = 8 * BitsToBytes(m_bitsLength);
-    m_owns = isOwns;
-    size_t t_bytesLength = BitsToBytes(m_bitsLength);
-    if (isClear)
-        memset(m_data, 0, t_bytesLength);
-}
-
-void BitArray::setData(unsigned char *data, int bitsLength, bool isClear, bool isOwns)
-{
-    if (m_owns && m_data != nullptr)
-        delete[] m_data;
-    m_data = data;
-    m_bitsLength = bitsLength;
-    m_bitsCapacity = 8 * BitsToBytes(m_bitsLength);
-    m_owns = isOwns;
-    size_t t_bytesLength = BitsToBytes(m_bitsLength);
-    if (isClear)
-        memset(m_data, 0, t_bytesLength);
-}
-
-bool BitArray::set(int position, bool bit, bool isAllowOutOfRange, bool isAllowOutOfSize, bool isAllowToInfinite)
-{
-    /**
-        整体思路：将position分为六个区间，(-INF,-m_len),[-m_len,0),
-            [0,m_len),[m_len,m_cap),[m_cap,c_max*m_cap),[c_max*m_cap,INF)
-        一定越界的范围:(-INF,-m_len)
-        越界与否取决于isAllowToInfinite：[c_max*m_cap,INF)
-        越界与否取决于isAllowedOutOfRange:[m_cap,c_max*m_cap)  及 isAllowToInfinite
-        越界与否取决于isAllowOutOfSize:[m_len,m_cap)    及 isAllowToInfinite
-        合法访问范围：[-m_len,0),[0,m_len),
-    **/
-    // position比 -(int)m_bitsLength 还小，或者需要扩张的倍数超出c_maxAllowedOutOfBound,此时一定越界
-    if (position < -(int)m_bitsLength || (position >= size_t(c_maxAllowedOutOfBound * m_bitsCapacity) && !isAllowToInfinite))
-    {
-        throw std::out_of_range("Out of range , This position is too larger!");
-    }
-    // 注意 isAllowToInfinite ， 如果这个值为 true，那么其他的条件开关将被忽略
-    // 如果不允许进行自动扩张，而访问位置超出 m_bitsCapacity
-    if (!isAllowOutOfRange && position >= m_bitsCapacity && !isAllowToInfinite)
-    {
-        throw std::out_of_range("Out of range , You are not allowed to automatically expanded memory!");
-    }
-    if (!isAllowOutOfSize && position >= m_bitsLength && !isAllowToInfinite)
-    {
-        throw std::out_of_range("Out of range , You are not allowed to amplification size automatically!");
-    }
-    // 以负数进行访问，修正position的实际位置,使得 [-m_len,0) -> [0,m_len)
-    if (position < 0)
-    {
-        position += m_bitsLength;
-    }
-    if (position < m_bitsLength)
-    {
-        // 访问位置没有超出目前的长度
-        return writeBit(m_data, position, bit);
-    }
-    else if (position >= m_bitsLength && position < m_bitsCapacity)
-    {
-        // 访问的位置已经超出了目前的长度，但是并没有超出实际的容量
-        m_bitsLength = position + 1;
-        return writeBit(m_data, position, bit);
-    }
-    else
-    {
-        size_t t_new_bitsLength = position + 1;
-        size_t t_new_bytesCapacity = BitsToBytes(c_increaseCapacity * t_new_bitsLength);
-        size_t t_new_bitsCapacity = 8 * t_new_bytesCapacity;
-        uchar *t_data = new uchar[t_new_bytesCapacity];
-        if (!t_data)
-        {
-            // 内存分配失败逻辑
-            throw std::bad_alloc(); //("can't allow memory!");
-        }
-        memset(t_data, 0, t_new_bytesCapacity);
-        memcpy(t_data, m_data, BitsToBytes(m_bitsCapacity));
-        if (m_owns)
-        {
+        if (m_owns && m_data != nullptr)
             delete[] m_data;
-        }
-        m_data = t_data;
-        m_bitsCapacity = t_new_bitsCapacity;
-        m_bitsLength = t_new_bitsLength;
-        m_owns = true;
-        return writeBit(m_data, position, bit);
     }
-}
 
-bool BitArray::get(int position)
-{
-    if (position >= m_bitsLength || position < -(int)m_bitsLength)
+    bool BitArray::operator==(BitArray &bits)
     {
-        // 访问越界,抛出异常
-        throw std::out_of_range("The location of the access is illegal!");
-    }
-    if (position < 0 && position >= -(int)m_bitsLength)
-    {
-        // 以负数进行访问，修正position的实际位置
-        position += m_bitsLength;
-    }
-    return readBit(m_data, position);
-}
-
-size_t BitArray::setBitSize(size_t newBitsLength)
-{
-    size_t origin_bitsLength = m_bitsLength;
-    if (newBitsLength <= m_bitsCapacity)
-    {
-        m_bitsLength = newBitsLength;
-    }
-    else
-    {
-        // 既然需要将大小扩充至newBitsLength，那么在newBitsLength-1 处赋false即可完成该功能
-        set(newBitsLength - 1, false, true, true, true);
-    }
-    return origin_bitsLength;
-}
-
-size_t BitArray::setBitCapacity(size_t newBitsCapacity)
-{
-    /**
-        整体思路：无论新的容量是多少，显然当与原来大小不一样时是需要进行扩容的
-        但是如果新容量比原来的长度还小，那么长度必须进行修改
-    **/
-    // 原来的容量必定为8的倍数
-    size_t origin_bytesCapacity = BitsToBytes(m_bitsCapacity);
-    size_t new_bytesCapacity = BitsToBytes(newBitsCapacity);
-    if (origin_bytesCapacity != new_bytesCapacity)
-    {
-        uchar *t_data = new uchar[new_bytesCapacity];
-        if (!t_data)
+        if (m_bitsLength != bits.m_bitsLength)
+            return false;
+        for (int i = 0; i < m_bitsLength; i++)
         {
-            // 内存分配失败逻辑
-            throw std::bad_alloc(); //("can't allow memory!");
+            if (get(i) != bits.get(i))
+            {
+                return false;
+            }
         }
-        memset(t_data, 0, new_bytesCapacity);
-        if (origin_bytesCapacity < new_bytesCapacity)
+        return true;
+    }
+
+    BitArray::BitArray(unsigned char *data, int bitsLength, bool isClear, bool isOwns)
+    {
+        m_data = data;
+        m_bitsLength = bitsLength;
+        m_bitsCapacity = 8 * BitsToBytes(m_bitsLength);
+        m_owns = isOwns;
+        size_t t_bytesLength = BitsToBytes(m_bitsLength);
+        if (isClear)
+            memset(m_data, 0, t_bytesLength);
+    }
+
+    void BitArray::setData(unsigned char *data, int bitsLength, bool isClear, bool isOwns)
+    {
+        if (m_owns && m_data != nullptr)
+            delete[] m_data;
+        m_data = data;
+        m_bitsLength = bitsLength;
+        m_bitsCapacity = 8 * BitsToBytes(m_bitsLength);
+        m_owns = isOwns;
+        size_t t_bytesLength = BitsToBytes(m_bitsLength);
+        if (isClear)
+            memset(m_data, 0, t_bytesLength);
+    }
+
+    bool BitArray::set(int position, bool bit, bool isAllowOutOfRange, bool isAllowOutOfSize, bool isAllowToInfinite)
+    {
+        /**
+            整体思路：将position分为六个区间，(-INF,-m_len),[-m_len,0),
+                [0,m_len),[m_len,m_cap),[m_cap,c_max*m_cap),[c_max*m_cap,INF)
+            一定越界的范围:(-INF,-m_len)
+            越界与否取决于isAllowToInfinite：[c_max*m_cap,INF)
+            越界与否取决于isAllowedOutOfRange:[m_cap,c_max*m_cap)  及 isAllowToInfinite
+            越界与否取决于isAllowOutOfSize:[m_len,m_cap)    及 isAllowToInfinite
+            合法访问范围：[-m_len,0),[0,m_len),
+        **/
+        // position比 -(int)m_bitsLength 还小，或者需要扩张的倍数超出c_maxAllowedOutOfBound,此时一定越界
+        if (position < -(int)m_bitsLength || (position >= size_t(c_maxAllowedOutOfBound * m_bitsCapacity) && !isAllowToInfinite))
         {
-            // 如果新容量比原来的容量大，那么全部复制
-            memcpy(t_data, m_data, origin_bytesCapacity);
+            throw std::out_of_range("Out of range , This position is too larger!");
+        }
+        // 注意 isAllowToInfinite ， 如果这个值为 true，那么其他的条件开关将被忽略
+        // 如果不允许进行自动扩张，而访问位置超出 m_bitsCapacity
+        if (!isAllowOutOfRange && position >= m_bitsCapacity && !isAllowToInfinite)
+        {
+            throw std::out_of_range("Out of range , You are not allowed to automatically expanded memory!");
+        }
+        if (!isAllowOutOfSize && position >= m_bitsLength && !isAllowToInfinite)
+        {
+            throw std::out_of_range("Out of range , You are not allowed to amplification size automatically!");
+        }
+        // 以负数进行访问，修正position的实际位置,使得 [-m_len,0) -> [0,m_len)
+        if (position < 0)
+        {
+            position += m_bitsLength;
+        }
+        if (position < m_bitsLength)
+        {
+            // 访问位置没有超出目前的长度
+            return writeBit(m_data, position, bit);
+        }
+        else if (position >= m_bitsLength && position < m_bitsCapacity)
+        {
+            // 访问的位置已经超出了目前的长度，但是并没有超出实际的容量
+            m_bitsLength = position + 1;
+            return writeBit(m_data, position, bit);
         }
         else
         {
-            // 如果新容量比原来的容量小，那么仅复制一部分
-            memcpy(t_data, m_data, new_bytesCapacity);
+            size_t t_new_bitsLength = position + 1;
+            size_t t_new_bytesCapacity = BitsToBytes(c_increaseCapacity * t_new_bitsLength);
+            size_t t_new_bitsCapacity = 8 * t_new_bytesCapacity;
+            uchar *t_data = new uchar[t_new_bytesCapacity];
+            if (!t_data)
+            {
+                // 内存分配失败逻辑
+                throw std::bad_alloc(); //("can't allow memory!");
+            }
+            memset(t_data, 0, t_new_bytesCapacity);
+            memcpy(t_data, m_data, BitsToBytes(m_bitsCapacity));
+            if (m_owns)
+            {
+                delete[] m_data;
+            }
+            m_data = t_data;
+            m_bitsCapacity = t_new_bitsCapacity;
+            m_bitsLength = t_new_bitsLength;
+            m_owns = true;
+            return writeBit(m_data, position, bit);
         }
-        if (m_owns)
-        {
-            delete[] m_data;
-        }
-        m_data = t_data;
-        m_bitsCapacity = BytesToBits(new_bytesCapacity);
-        if (m_bitsLength > m_bitsCapacity)
-        {
-            m_bitsLength = m_bitsCapacity;
-        }
-        m_owns = true;
     }
-    return BytesToBits(origin_bytesCapacity);
-}
 
-bool BitArray::setOwns(bool owns)
-{
-    bool r = m_owns;
-    m_owns = owns;
-    return r;
-}
-
-/*
-    在map的position位置写入bit
-*/
-bool writeBit(unsigned char *map, int position, bool bit)
-{
-    // sub表示在szMap中的下标，pos表示在该位置中相应的比特位
-    int sub = (position) / 8;
-    int pos = 7 - (position) % 8;
-    if (bit)
+    bool BitArray::get(int position)
     {
-        map[sub] |= 1 << pos; // 打开位开关
+        if (position >= m_bitsLength || position < -(int)m_bitsLength)
+        {
+            // 访问越界,抛出异常
+            throw std::out_of_range("The location of the access is illegal!");
+        }
+        if (position < 0 && position >= -(int)m_bitsLength)
+        {
+            // 以负数进行访问，修正position的实际位置
+            position += m_bitsLength;
+        }
+        return readBit(m_data, position);
     }
-    else
+
+    size_t BitArray::setBitSize(size_t newBitsLength)
     {
-        map[sub] &= ~(1 << pos); // 关闭位开关
+        size_t origin_bitsLength = m_bitsLength;
+        if (newBitsLength <= m_bitsCapacity)
+        {
+            m_bitsLength = newBitsLength;
+        }
+        else
+        {
+            // 既然需要将大小扩充至newBitsLength，那么在newBitsLength-1 处赋false即可完成该功能
+            set(newBitsLength - 1, false, true, true, true);
+        }
+        return origin_bitsLength;
     }
-    return true;
-}
 
-/*
-    读取map的position位置的bit数据
-*/
-bool readBit(unsigned char *map, int position)
-{
-    // sub 代表 szMap中对应的下标，范围是[0,bitmapLength) ；pos为相应的bit位置，范围是[0,8)
-    int sub = (position) / 8;
-    int pos = 7 - (position) % 8;
-    return bool((map[sub] >> pos) & 1);
-}
+    size_t BitArray::setBitCapacity(size_t newBitsCapacity)
+    {
+        /**
+            整体思路：无论新的容量是多少，显然当与原来大小不一样时是需要进行扩容的
+            但是如果新容量比原来的长度还小，那么长度必须进行修改
+        **/
+        // 原来的容量必定为8的倍数
+        size_t origin_bytesCapacity = BitsToBytes(m_bitsCapacity);
+        size_t new_bytesCapacity = BitsToBytes(newBitsCapacity);
+        if (origin_bytesCapacity != new_bytesCapacity)
+        {
+            uchar *t_data = new uchar[new_bytesCapacity];
+            if (!t_data)
+            {
+                // 内存分配失败逻辑
+                throw std::bad_alloc(); //("can't allow memory!");
+            }
+            memset(t_data, 0, new_bytesCapacity);
+            if (origin_bytesCapacity < new_bytesCapacity)
+            {
+                // 如果新容量比原来的容量大，那么全部复制
+                memcpy(t_data, m_data, origin_bytesCapacity);
+            }
+            else
+            {
+                // 如果新容量比原来的容量小，那么仅复制一部分
+                memcpy(t_data, m_data, new_bytesCapacity);
+            }
+            if (m_owns)
+            {
+                delete[] m_data;
+            }
+            m_data = t_data;
+            m_bitsCapacity = BytesToBits(new_bytesCapacity);
+            if (m_bitsLength > m_bitsCapacity)
+            {
+                m_bitsLength = m_bitsCapacity;
+            }
+            m_owns = true;
+        }
+        return BytesToBits(origin_bytesCapacity);
+    }
 
-Bit::Bit(BitArray *bits, int position)
-{
-    m_bits = bits;
-    m_position = position;
-}
+    bool BitArray::setOwns(bool owns)
+    {
+        bool r = m_owns;
+        m_owns = owns;
+        return r;
+    }
 
-Bit &Bit::operator=(bool bit)
-{
-    m_bits->set(m_position, bit, true);
-    return *this;
-}
+    /*
+        在map的position位置写入bit
+    */
+    bool writeBit(unsigned char *map, int position, bool bit)
+    {
+        // sub表示在szMap中的下标，pos表示在该位置中相应的比特位
+        int sub = (position) / 8;
+        int pos = 7 - (position) % 8;
+        if (bit)
+        {
+            map[sub] |= 1 << pos; // 打开位开关
+        }
+        else
+        {
+            map[sub] &= ~(1 << pos); // 关闭位开关
+        }
+        return true;
+    }
 
-Bit::operator bool()
-{
-    return m_bits->get(m_position);
+    /*
+        读取map的position位置的bit数据
+    */
+    bool readBit(unsigned char *map, int position)
+    {
+        // sub 代表 szMap中对应的下标，范围是[0,bitmapLength) ；pos为相应的bit位置，范围是[0,8)
+        int sub = (position) / 8;
+        int pos = 7 - (position) % 8;
+        return bool((map[sub] >> pos) & 1);
+    }
+
+    Bit::Bit(BitArray *bits, int position)
+    {
+        m_bits = bits;
+        m_position = position;
+    }
+
+    Bit &Bit::operator=(bool bit)
+    {
+        m_bits->set(m_position, bit, true);
+        return *this;
+    }
+
+    Bit::operator bool()
+    {
+        return m_bits->get(m_position);
+    }
 }
