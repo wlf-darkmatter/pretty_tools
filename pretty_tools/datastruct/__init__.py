399124312514d385d66004b019adb4152470fd37
docker <docker@x-contion.top>
2024-01-31 17:35:27 +0800

    .

--- pretty_tools/datastruct/__init__.py
+++ pretty_tools/datastruct/__init__.py
@@ -5,11 +5,9 @@
 
 from .general_ann import GeneralAnn
 
-from .graph_enhance import merge_affinity
 from .multi_index_dict import mdict
 from .track_graph import TrackCameraGraph
 from .track_instance import TrackCameraInstances, TrackInstance
-
 
 from . import bbox_convert_np
 from .bbox_convert_np import ltrb_to_ltwh as np_ltrb_to_ltwh
