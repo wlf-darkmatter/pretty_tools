e6011d72a1bbd9db2b2eb3624af38bc696b31d19
docker <docker@x-contion.top>
2024-04-29 20:10:01 +0800

    。

--- pretty_tools/__init__.py
+++ pretty_tools/__init__.py
@@ -4,3 +4,4 @@ from .echo import X_Logging
 from .resources import *
 
 PATH_PRETTY = Path(__file__).parent
+VERSION = "0.2.0"
--- pretty_tools/echo/__init__.py
+++ pretty_tools/echo/__init__.py
@@ -1,10 +1,11 @@
 from .echo_resource import Echo_Resource
-from .message import message_WeChat_send
+
+# from .message import message_WeChat_send #* 加载request比较耗时，这个不默认加载
 from .x_logging import X_Logging, build_logging
 from .x_progress import X_Progress, Safe_Progress
 from .x_table import X_Table
 from .x_timer import X_Timer
 
-__all__ = ["Echo_Resource", "message_WeChat_send", "X_Logging", "X_Table", "X_Progress", "X_Timer"]
+__all__ = ["Echo_Resource", "X_Logging", "X_Table", "X_Progress", "X_Timer"]
 
 classes = __all__
